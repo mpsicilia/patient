1e54c361cbe293457744f35f80459065ba9aadcd
luchiparejo <luchiparejo@10.60.58.88>
2018-11-15 12:52:03 +0100

    changes

--- src/connection/SendPatient.java
+++ src/connection/SendPatient.java
@@ -3,14 +3,18 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package patient;
+package connection;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.ObjectOutputStream;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.time.LocalDateTime;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import patient.Patient;
 
 /**
  *
@@ -18,17 +22,14 @@ import java.util.logging.Logger;
  */
 public class SendPatient {
 
-    public static void main(String args[]) {
+    public static void main(String args[]) throws IOException {
         OutputStream outputStream = null;
         ObjectOutputStream objectOutputStream = null;
         Socket socket = null;
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 
         //int numberOfPatients;
         //while (){}
-        Patient[] patients = new Patient[2];
-        patients[0] = new Patient("María Plaza", 20, (float) 40.8);
-        patients[1] = new Patient("Claudia Saiz", 21, (float) 32.5);
-
         try {
             socket = new Socket("localhost", 9000);
             outputStream = socket.getOutputStream();
@@ -37,6 +38,63 @@ public class SendPatient {
             System.exit(-1);
             Logger.getLogger(SendPatient.class.getName()).log(Level.SEVERE, null, ex);
         }
+
+        String[] symptoms = null;
+        String[] signs = null;
+        boolean stop = true;
+        int counter = 0;
+        String stopsymptoms = null;
+        String stopsigns = null;
+
+        System.out.println("connection stablished");
+        System.out.println("introduce your name ");
+        String name = bf.readLine();
+        System.out.println("introduce your age");
+        int age = bf.read();
+        System.out.println("introduce your weight (kg)");
+        float weight = bf.read();
+        System.out.println("introduce your height (m)");
+        float height = bf.read();
+        //date
+        System.out.println("introduce your symptoms... ");
+        System.out.println("when you finish introducing your symptoms type 'x'");
+
+        while (stop) {
+
+            stopsymptoms = bf.readLine();
+            if (stopsymptoms.equals('x')) {
+                stop = false;
+                break;
+            } else {
+                symptoms[counter] = stopsymptoms;
+                counter++;
+            }
+
+        }
+        stop=true;
+        counter=0;
+        System.out.println("introduce your signs... ");
+        System.out.println("when you finish introducing your signs type 'x'");
+
+        while (stop) {
+
+            stopsigns = bf.readLine();
+            if (stopsigns.equals('x')) {
+                stop = false;
+                break;
+            } else {
+                signs[counter] = stopsigns;
+                counter++;
+            }
+
+        }
+        LocalDateTime date = LocalDateTime.now();
+        Patient patient=new Patient(name, age, weight, height, date, signs, symptoms);
+        
+
+        /*Patient[] patients = new Patient[2];
+        patients[0] = new Patient("María Plaza", 20, (float) 40.8);
+        patients[1] = new Patient("Claudia Saiz", 21, (float) 32.5);*/
 
         try {
             objectOutputStream = new ObjectOutputStream(outputStream);
--- src/patient/Patient.java
+++ src/patient/Patient.java
@@ -1,11 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package patient;
 
+
 import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.Arrays;
 
 /**
  *
@@ -13,22 +11,28 @@ import java.io.Serializable;
  */
 public class Patient implements Serializable{
 private static final long serialVersionUID = -6291904286218553733L;
-
-    private String name;
-    private int age;
-    private float weight;
+//claudia 
+//saiz 
+    private final String name;
+    private final int age;
+    private final float weight;
+    private final float height;
+    private final LocalDateTime monitoring;
+    private final String[] signs;
+    private final String[] symptoms;
     
-    public Patient (String namePatient, int agePatient, float weightPatient) {
+    public Patient (String namePatient, int agePatient, float weightPatient, float heightPatient, LocalDateTime monitoringData, String[] sign, String[] symptom)  {
         this.name = namePatient;
         this.age = agePatient;
         this.weight = weightPatient;
+        this.height = heightPatient;
+        this.monitoring = monitoringData;
+        this.signs = sign;
+        this.symptoms = symptom;
     }
 
     @Override
     public String toString() {
-        return ("Name: " + name + "\nAge: " + age
-                + "\nWeight: " + weight);
-    }
-    
-    
+        return ("Name: " + name + "\nAge: " + age+ "\nWeight: " + weight+ "\nHeight: " +height+ "\nMonitoring: " + monitoring+ "\nSings: "+Arrays.toString(signs)+ "\nSymptoms: "+Arrays.toString(symptoms));
+    } 
 }
