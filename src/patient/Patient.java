875f49820ab73c9298e896d97e085ad4ffd9bd48
luchiparejo <luchiparejo@10.60.58.109>
2018-11-26 18:40:52 +0100

    patient

--- src/patient/Patient.java
+++ src/patient/Patient.java
@@ -21,11 +21,88 @@ public class Patient implements Serializable {
     private float weight;
     private float height;
     private LocalDateTime monitoring;
-    private final String[] signs;
-    private final String[] symptoms;
+    private final String[] signs_symptoms;
     public int[][] databitalino = new int[2][10000];
     private String username;
     private String passwordPat;
+    private boolean sevheadache;
+    private boolean fatigue;
+    private boolean chestpain;
+    private boolean diffdifficultybreath;
+    private boolean irregheartbeat;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setWeight(float weight) {
+        this.weight = weight;
+    }
+
+    public void setHeight(float height) {
+        this.height = height;
+    }
+
+    public void setMonitoring(LocalDateTime monitoring) {
+        this.monitoring = monitoring;
+    }
+
+    public void setDatabitalino(int[][] databitalino) {
+        this.databitalino = databitalino;
+    }
+
+    public void setSevheadache(boolean sevheadache) {
+        this.sevheadache = sevheadache;
+    }
+
+    public void setFatigue(boolean fatigue) {
+        this.fatigue = fatigue;
+    }
+
+    public void setChestpain(boolean chestpain) {
+        this.chestpain = chestpain;
+    }
+
+    public void setBreath(boolean diffdifficultybreath) {
+        this.diffdifficultybreath = diffdifficultybreath;
+    }
+
+    public void setTachychardia(boolean irregheartbeat) {
+        this.irregheartbeat = irregheartbeat;
+    }
+
+    public String[] getSigns_symptoms() {
+        return signs_symptoms;
+    }
+
+    public boolean isSevheadache() {
+        return sevheadache;
+    }
+
+    public boolean isFatigue() {
+        return fatigue;
+    }
+
+    public boolean isChestpain() {
+        return chestpain;
+    }
+
+    public boolean isBreath() {
+        return diffdifficultybreath;
+    }
+
+    public boolean isTachychardia() {
+        return irregheartbeat;
+    }
+ 
 
     public static long getSerialVersionUID() {
         return serialVersionUID;
@@ -73,35 +150,42 @@ public class Patient implements Serializable {
     }
 
     public String[] getSigns() {
-        return signs;
+        return signs_symptoms;
     }
 
-    public String[] getSymptoms() {
-        return symptoms;
-    }
+
 
     public void setDataBitalino(int[][] databitalino) {
         this.databitalino = databitalino;
     }
 
-    public Patient(String namePatient, String surnameP, int agePatient, float weightPatient, float heightPatient, LocalDateTime monitoringData, String[] sign, String[] symptom) {
+    public Patient(String namePatient, String surnameP, int agePatient, float weightPatient, float heightPatient, LocalDateTime monitoringData,boolean sevheadache,  boolean fatigue, boolean chestpain, boolean diffdifficultybreath, boolean irregheartbeat, String[] signs_symptoms) {
         this.name = namePatient;
         this.surname = surnameP;
         this.age = agePatient;
         this.weight = weightPatient;
         this.height = heightPatient;
         this.monitoring = monitoringData;
-        this.signs = sign;
-        this.symptoms = symptom;
+        this.sevheadache = sevheadache;
+        this.fatigue = fatigue;
+        this.chestpain=chestpain;
+        this.diffdifficultybreath=diffdifficultybreath;
+        this.irregheartbeat=irregheartbeat;
+        this.signs_symptoms = signs_symptoms;
+      
     }
 
-    public Patient(String namePatient, String surnameP, float weightPatient, LocalDateTime monitoringData, String[] sign, String[] symptom) {
-        this.name = namePatient;
-        this.surname = surnameP;
+    public Patient(float weightPatient, LocalDateTime monitoringData,boolean sevheadache,  boolean fatigue, boolean chestpain, boolean diffdifficultybreath, boolean irregheartbeat,String[] signs_symptoms) {
+      
         this.weight = weightPatient;
         this.monitoring = monitoringData;
-        this.signs = sign;
-        this.symptoms = symptom;
+        this.sevheadache = sevheadache;
+        this.fatigue = fatigue;
+        this.chestpain=chestpain;
+        this.diffdifficultybreath=diffdifficultybreath;
+        this.irregheartbeat=irregheartbeat;
+        this.signs_symptoms = signs_symptoms;
+  
     }
 
     public int[][] getDatabitalino() {
@@ -110,7 +194,7 @@ public class Patient implements Serializable {
 
     @Override
     public String toString() {
-        return (name + " " + surname + "\nAge: " + age + "\nWeight: " + weight + "\nHeight: " + height + "\nMonitoring: " + monitoring + "\nSings: " + Arrays.toString(signs) + "\nSymptoms: " + Arrays.toString(symptoms));
+        return (name + " " + surname + "\nAge: " + age + "\nWeight: " + weight + "\nHeight: " + height + "\nMonitoring: " + monitoring + "\nSevere headache: "+ sevheadache+ "\nchestpain: "+ chestpain+ "\nfatigue: "+ fatigue +"\nirregheartbeat: "+ irregheartbeat+"\ndiffdifficultybreath: "+ diffdifficultybreath+"\nAdditional information: " + Arrays.toString(signs_symptoms) );
     }
 
     public static boolean RegularExp(String name) {
